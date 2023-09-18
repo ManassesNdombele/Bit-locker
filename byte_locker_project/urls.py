3480a33ebfb28b1121dbc912c6fc02cbe69ecc35
ManassesNdombele <manassesndombele@gmail.com>
2023-09-18 21:51:44 +0200

    Conexão de Front-End com Back-End
    Conectei o front-end com o back-end ou seja o template principal e os arquivos do diretório static

--- byte_locker_project/urls.py
+++ byte_locker_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from byte_locker_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home_view')
 ]
