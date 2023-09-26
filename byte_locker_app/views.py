1d492acbbd941bc313877d02ef8c14c918288232
ManassesNdombele <manassesndombele@gmail.com>
2023-09-26 06:17:07 +0200

    Novas rotas para os novos templates
    Adicionei novas rotas para os novos templates

--- byte_locker_app/views.py
+++ byte_locker_app/views.py
@@ -8,38 +8,20 @@ def home(request):
 
     return render(request, 'index.html')
 
-def generate_password(to: list, max_length: int):
-    char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!"#$%&/_-@'
-    chars = choices(char_list, k=max_length)
-    for c in range(0, 5):
-        to.append(''.join(chars))
-    return to
-
 def password_generator(request):
-    response = {
-        'DATA_SENDED': True,
-        'ERROR': False,
-        'ERROR_DESC': '',
-        'FIRST_PASSWD': None,
-        'SECOND_PASSWD': None,
-        'THIRD_PASSWD': None,
-        'FOURTH_PASSWD': None,
-        'FIFTH_PASSWD': None
-    }
+    return render(request, 'passwd_gen.html')
 
-    if request.method == 'POST':
-        software_name = request.POST.get('software-name-input')
-        email_or_user = request.POST.get('email-user-name-input')
-        char_length = request.POST.get('char-length-input')
-        if software_name == '' or email_or_user == '' or char_length == '':
-            response['ERROR'] = True
-            response['ERROR_DESC'] = 'Todos os campos são obrigatórios!'
-            return render(request, 'index.html', response)
+def account_configs(request):
+    return render(request, 'account-configs.html')
 
-        else:
-            response['FIRST_PASSWD'] = generate_password(to=[], max_length=int(char_length))[0]
-            response['SECOND_PASSWD'] = generate_password(to=[], max_length=int(char_length))[1]
-            response['THIRD_PASSWD'] = generate_password(to=[], max_length=int(char_length))[2]
-            response['FOURTH_PASSWD'] = generate_password(to=[], max_length=int(char_length))[3]
-            response['FIFTH_PASSWD'] = generate_password(to=[], max_length=int(char_length))[4]
-            return render(request, 'index.html', response)
+def authentication(request):
+    return render(request, 'authentication.html')
+
+def help_center(request):
+    return render(request, 'help-center.html')
+
+def saved_accounts(request):
+    return render(request, 'saved-accounts.html')
+
+def saved_passwords(request):
+    return render(request, 'saved-passwd.html')
--- byte_locker_project/urls.py
+++ byte_locker_project/urls.py
@@ -5,5 +5,10 @@ from byte_locker_app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home_view'),
-    path('password-generator/', password_generator, name='passwd_gen')
+    path('password-generator/', password_generator, name='passwd_gen'),
+    path('account-configs/', account_configs, name='account_configs'),
+    path('authentication/', authentication, name='authentication'),
+    path('help-center', help_center, name='help_center'),
+    path('saved-accounts/', saved_accounts, name='saved_accounts'),
+    path('saved-passwords/', saved_passwords, name='saved_passwords')
 ]
