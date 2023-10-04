9a6dd3d6c37bafd9df019dfa1626fe3b260f90a4
ManassesNdombele <manassesndombele@gmail.com>
2023-10-04 19:56:05 +0200

    Geração de senha
    Desenvolvi a funcionalidade de geração de senha com base em parâmetros configurados pelo usuário através de interface

--- byte_locker_app/views.py
+++ byte_locker_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from random import choices
 
 def home(request):
     return render(request, 'index.html')
 
 def password_generator(request):
-    return render(request, 'passwd-gen.html')
+    return render(request, 'passwd-gen.html', {'DATA_SENDED': False, 'ERROR': False, 'MESSAGE': ''})
 
 def account_configs(request):
     return render(request, 'account-configs.html')
@@ -20,3 +21,35 @@ def saved_accounts(request):
 
 def saved_passwords(request):
     return render(request, 'saved-passwd.html')
+
+def generate_password(request):
+    if request.method == 'POST':
+        program_name = request.POST.get('software-name-input')
+        email = request.POST.get('email-input')
+        user_name = request.POST.get('user-name-input')
+        char_length = request.POST.get('char-length-input')
+        upper_letters_check = request.POST.get('upper-letters-checkbox')
+        lower_letters_check = request.POST.get('lower-letters-checkbox')
+        numbers_check = request.POST.get('numbers-checkbox')
+        account_description = request.POST.get('account-description-textbox')
+        if str(email) == '' and str(user_name) == '':
+            return render(request, 'passwd-gen.html', {'DATA_SENDED': False, 'ERROR': True, 'MESSAGE': 'Tens de preencher o campo de email ou o campo de nome de usuário ou os dois campos!'})
+
+        else:
+            upper_letters = ''
+            lower_letters = ''
+            numbers_chars = ''
+            if str(upper_letters_check) == 'on':
+                upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            
+            if str(lower_letters_check) == 'on':
+                lower_letters = 'abcdefghijklmnopqrstuvwxyz'
+            
+            if str(numbers_check) == 'on':
+                numbers_chars = '0123456789'
+
+            base_chars = f'{upper_letters}{lower_letters}{numbers_chars}!@#$%&*-'
+            passwd_char_list = choices(base_chars, k=int(char_length))
+            password = ''.join(passwd_char_list)
+            print(password)
+            return render(request, 'index.html')
--- byte_locker_project/urls.py
+++ byte_locker_project/urls.py
@@ -1,14 +1,13 @@
-from django.contrib import admin
 from django.urls import path
 from byte_locker_app.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home, name='home_view'),
     path('password-generator/', password_generator, name='passwd_gen'),
     path('account-configs/', account_configs, name='account_configs'),
     path('authentication/', authentication, name='authentication'),
     path('help-center/', help_center, name='help_center'),
     path('saved-accounts/', saved_accounts, name='saved_accounts'),
-    path('saved-passwords/', saved_passwords, name='saved_passwords')
+    path('saved-passwords/', saved_passwords, name='saved_passwords'),
+    path('generate-passwd/', generate_password, name='generate_password')
 ]
